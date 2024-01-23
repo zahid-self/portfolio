5eb1ac11daa8f5484521eed67bfe4851bd191d24
Zahid Hasan <mdjahidhasan920@gmail.com>
2024-01-23 23:43:09 +0600

    Making portfolio v2, design navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,13 @@ import Hero from '@/components/Hero'
 import Navigation from '@/components/Navigation'
 import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
+import Navbar from '@/components/v2/Navbar'
 
 
 export default function Home() {
   return (
     <>
-      <div className='w-full h-[694px] flex-shrink-0' style={{ backgroundImage: 'linear-gradient(rgba(7, 0, 12, 0.5), rgba(7, 0, 12, 0.8)), url(./hero_bg.jpg)', backgroundPosition: 'center', backgroundSize: 'cover' }}>
-        <Navigation />
-        <Hero />
-      </div>
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <Navbar />
     </>
   )
 }
---
+++ src/components/v2/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='container mx-auto justify-between'>
+      <div className='flex w-full h-[60px] p-[16px] justify-between items-center flex-shrink-0 rounded-[12px] bg-[rgba(24,_24,_29,_0.30)] backdrop-filter backdrop-blur-[2px]'>
+        <div className='flex items-center gap-[40px]'>
+          <h1>Z</h1>
+          <Link className='text-[#C5C5C5] text-[16px] not-italic font-medium leading-[normal]' href={'#'}>About</Link>
+          <Link className='text-[#C5C5C5] text-[16px] not-italic font-medium leading-[normal]' href={'#'}>Work</Link>
+          <Link className='text-[#C5C5C5] text-[16px] not-italic font-medium leading-[normal]' href={'#'}>Notebook</Link>
+          <Link className='text-[#C5C5C5] text-[16px] not-italic font-medium leading-[normal]' href={'#'}>Contact</Link>
+          <Link className='text-[#C5C5C5] text-[16px] not-italic font-medium leading-[normal]' href={'#'}>More</Link>
+        </div>
+        <div className='flex items-center justify-center gap-[24px]'>
+          <div className='flex w-[24px] h-[24px] flex-col justify-center items-center gap-[12px]'>
+            <Image src={'./linkedin.svg'} width={22} height={22} alt='Linkedin' />
+          </div>
+          <div className='flex w-[24px] h-[24px] flex-col justify-center items-center gap-[12px]'>
+            <Image src={'./x.svg'} width={22} height={22} alt='Linkedin' />
+          </div>
+          <div className='flex w-[24px] h-[24px] flex-col justify-center items-center gap-[12px]'>
+            <Image src={'./github.svg'} width={22} height={22} alt='Linkedin' />
+          </div>
+          <div className='flex w-[24px] h-[24px] flex-col justify-center items-center gap-[12px]'>
+            <Image src={'./linkedin.svg'} width={22} height={22} alt='Linkedin' />
+          </div>
+          <div className='w-[1px] h-[24px] bg-[rgba(255,_255,_255,_0.25)]'></div>
+          <div className='flex w-[24px] h-[24px] flex-col justify-center items-center gap-[12px]'>
+            <Image src={'./sun.svg'} width={22} height={22} alt='Linkedin' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
